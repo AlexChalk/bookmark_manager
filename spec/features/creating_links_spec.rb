d8e8d3f24d87b068b8c83a068c976bd06a99bd5c
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-05-02 16:00:06 +0100

    adds creating_links_spec with failing test

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,7 +1,8 @@
 feature 'creating a link' do
   scenario 'create a link' do
     visit '/links'
-    fill_in(link) 'new link'
+    fill_in('link_name', with:'google')
+    fill_in('link_url', with: 'http://www.google.com')
     click_button 'Create new link'
   end
 end
