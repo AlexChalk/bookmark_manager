ffa01d583a8413806d17c9396c6bb172bda9d0cf
Alex Chalk <alexchalk17@gmail.com>
2017-05-02 11:17:58 -0400

    create_link page: implement basic link saving functionality

--- app/app.rb
+++ app/app.rb
@@ -4,10 +4,21 @@ require './models/link'
 
 class BookmarkManager < Sinatra::Base
 
+  before { @links = Link.all }
+
   get '/links' do
-    @links = Link.all
-    erb :links
+    erb :'links/index'
   end
+
+  post '/links' do
+    @link = Link.create(title: params[:link_name], url: params[:link_url])
+    redirect '/links'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+    
 
   run! if app_file == $0
 end
--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,8 +1,15 @@
 feature 'creating a link' do
-  scenario 'create a link' do
-    visit '/links'
+  scenario 'I can create a link' do
+    visit '/links/new'
     fill_in('link_name', with:'google')
     fill_in('link_url', with: 'http://www.google.com')
     click_button 'Create new link'
+
+    expect(current_path).to eq '/links'
+
+    within('ul#links') do
+      expect(page).to have_content 'google'
+      expect(page).to have_content 'http://www.google.com'
+    end
   end
 end
