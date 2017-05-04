71e19c2538f8524f82f626aa696f93abd1b63564
Alex Chalk <alexchalk17@gmail.com>
2017-05-04 07:58:27 -0400

    Extract data_mapper config to separate file

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,6 @@
 ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
-#require_relative 'data_mapper_setup'
-require './models/link'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   before { @links = Link.all }
